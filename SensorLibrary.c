8edb9b60e918983af24e7b73fa655cae976f5935
Kevin Cooper <boo2atfoo@gmail.com>
2013-12-05 11:30:11 -0700

    Tried adding the interrupts to give the sensor time to get a reading.  However, the robot is still only getting 0x01b4 values.

--- SensorLibrary.c
+++ SensorLibrary.c
@@ -25,15 +25,22 @@ char isRightClose()
 //P1.4 - A4
 char isCenterClose()
 {
-      ADC10CTL0 &= ~ENC;
-      ADC10CTL1 &= ~INCH_5;
-	  ADC10CTL1 |= INCH_4;                       // input A5
+	  ADC10CTL0 = ADC10SHT_3 + ADC10ON + ADC10IE; // ADC10ON, interrupt enabled
+	  ADC10CTL1 = INCH_4;                       // input A4
 	  ADC10AE0 |= BIT4;                         // PA.1 ADC option select
-	  ADC10CTL1 |= ADC10SSEL1|ADC10SSEL0;       // Select SMCLK
-	  ADC10CTL0 |= ENC + ADC10SC;				// Sampling and conversion start
+	  ADC10CTL1 |= ADC10SSEL1|ADC10SSEL0;                // Select SMCLK
+	  ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
+	  __delay_cycles(64);
+      __bis_SR_register(CPUOFF + GIE);
 	  if (ADC10MEM < 0x1FF){
 		  return TRUE;
 	  }else{
 		  return FALSE;
 	  }
+}
+
+#pragma vector=ADC10_VECTOR
+__interrupt void ADC10_ISR(void)
+{
+  __bic_SR_register_on_exit(CPUOFF);        // Clear CPUOFF bit from 0(SR)
 }
