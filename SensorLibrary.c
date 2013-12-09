dfa7dd57861d5835bcfefb286ca881d0ae073c81
Kevin Cooper <boo2atfoo@gmail.com>
2013-12-09 11:06:25 -0700

    Not too sure what is different, but it says it is.  Commit's awayyyyy.......

--- SensorLibrary.c
+++ SensorLibrary.c
@@ -21,7 +21,7 @@ char isLeftClose()
 	  {
 	    ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
 	    __bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
-	    if (ADC10MEM < 0x1FF)
+	    if (ADC10MEM < 0x250)
 	      return TRUE;                       // Clear P1.0 LED off
 	    else
 	      return FALSE;                        // Set P1.0 LED on
@@ -32,7 +32,7 @@ char isRightClose()
 {
 	  ADC10CTL0 &= ~ENC;
 	  ADC10CTL0 = ADC10SHT_3 + ADC10ON + ADC10IE; // ADC10ON, interrupt enabled
-	  ADC10CTL1 = INCH_5;                       // input A4
+	  ADC10CTL1 = INCH_5;                       // input A5
 	  ADC10AE0 |= BIT5;                         // PA.1 ADC option select
 	  ADC10CTL1 |= ADC10SSEL1|ADC10SSEL0;                // Select SMCLK
 
@@ -40,7 +40,7 @@ char isRightClose()
 	  {
 	    ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
 	    __bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
-	    if (ADC10MEM < 0x1FF)
+	    if (ADC10MEM < 0x250)
 	      return TRUE;                       // Clear P1.0 LED off
 	    else
 	      return FALSE;                        // Set P1.0 LED on
